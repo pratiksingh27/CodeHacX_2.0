63d7519823d1c234c4f63f57966004d2a34ef204
pratiksingh27 <pratik27march@gmail.com>
2024-04-21 02:59:55 +0530

    changing data in firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,23 +5,18 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage"; 
 // import { getAnalytics } from "firebase/analytics";
 
-console.log(import.meta.env.REACT_SOME_KEY)
+console.log(import.meta.env.VITE_SOME_KEY);
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
 
 const firebaseConfig = {
-  /* apiKey: "AIzaSyCTK0uG2GLeUMqSqSHDsG5kWXdh73aPoS0",
-  authDomain: "codehacx-5af2e.firebaseapp.com",
-  projectId: "codehacx-5af2e",
-  storageBucket: "codehacx-5af2e.appspot.com",
-  messagingSenderId: "536705295612",
-  appId: "1:536705295612:web:b376394ccf19cd060be379",
-  measurementId: "G-459FSWQLX0" */
-  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
-  // measurementId: import.meta.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
